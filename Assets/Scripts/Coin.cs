1c28480eb6e7d43513f75fc05dba86195c962c18
raulcuth <raul.cuth@gmail.com>
2019-04-26 08:49:43 +0300

    added coin prefab and material, raised env light

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour {
+    public static int coinCount = 0;
+    // Start is called before the first frame update
+    void Start() {
+        ++Coin.coinCount;
+    }
+
+    private void OnTriggerEnter(Collider collider) {
+        if ( collider.CompareTag("Player")) {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnDestroy() {
+        --Coin.coinCount;
+        if (Coin.coinCount <= 0) {
+
+        }
+    }
+
+}
