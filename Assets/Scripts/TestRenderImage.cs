fcc5f60aafdc4122baac425ed86054cdb143a4e8
raulcuth <raul.cuth@gmail.com>
2019-05-29 18:51:40 +0300

    added grayscale screen effect

--- Assets/Scripts/TestRenderImage.cs
+++ Assets/Scripts/TestRenderImage.cs
@@ -1,8 +1,7 @@
 ﻿using UnityEngine;
 
 [ExecuteInEditMode]
-public class TestRenderImage : MonoBehaviour
-{
+public class TestRenderImage : MonoBehaviour {
     Material ScreenMat {
         get {
             if (screenMat == null) {
@@ -13,19 +12,34 @@ public class TestRenderImage : MonoBehaviour
         }
     }
 
-    public Shader curShader;
+    public Shader curShader = null;
     public float greyscaleAmount = 1.0f;
     private Material screenMat;
 
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        if (!curShader && !curShader.isSupported) {
+            enabled = false;
+        }
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        greyscaleAmount = Mathf.Clamp(greyscaleAmount, 0.0f, 1.0f);
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination) {
+        if (curShader != null) {
+            ScreenMat.SetFloat("_Luminosity", greyscaleAmount);
+            Graphics.Blit(source, destination, screenMat);
+        } else {
+            Graphics.Blit(source, destination);
+        }
+    }
+
+    private void OnDisable() {
+        if (screenMat) {
+            DestroyImmediate(screenMat);
+        }
     }
 }
