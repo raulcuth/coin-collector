0a367268c1a5d9233c90fd7d865cef42aa28f478
raulcuth <raul.cuth@gmail.com>
2019-06-02 16:21:31 +0300

    added velocity macth and jump point scripts

---
+++ Assets/Scripts/Movement/Jump/JumpPoint.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class JumpPoint {
+    public Vector3 jumpLocation;
+    public Vector3 landingLocation;
+
+    //the change in position from jump to landing
+    public Vector3 deltaPosition;
+
+    public JumpPoint() : this(Vector3.zero, Vector3.zero) {
+
+    }
+
+    public JumpPoint(Vector3 jumpLocation, Vector3 landingLocation) {
+        this.jumpLocation = jumpLocation;
+        this.landingLocation = landingLocation;
+        this.deltaPosition = this.landingLocation - this.jumpLocation;
+    }
+}
---
+++ Assets/Scripts/Movement/Jump/VelocityMatch.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class VelocityMatch : AgentBehaviour {
+    public float timeToTarget = 0.1f;
+
+    public override Steering GetSteering() {
+        Steering steering = new Steering();
+        steering.linear = target.GetComponent<Agent>().velocity - agent.velocity;
+        steering.linear /= timeToTarget;
+        if (steering.linear.magnitude > agent.maxAccel) {
+            steering.linear = steering.linear.normalized * agent.maxAccel;
+        }
+        steering.angular = 0.0f;
+        return steering;
+    }
+}
