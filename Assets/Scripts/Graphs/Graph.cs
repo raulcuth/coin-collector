e4077c63f23466b4937b85550a947abfc0bed641
raulcuth <raul.cuth@gmail.com>
2019-06-04 22:30:24 +0300

    added binary heap implementation with priority queue and implemented dijkstra's algorithm

--- Assets/Scripts/Graphs/Graph.cs
+++ Assets/Scripts/Graphs/Graph.cs
@@ -133,6 +133,64 @@ public class Graph : MonoBehaviour {
         return new List<Vertex>();
     }
 
+    public List<Vertex> GetPathDijkstra(GameObject srcObj, GameObject dstObj) {
+        if (srcObj == null || dstObj == null) {
+            return new List<Vertex>();
+        }
+        Vertex src = GetNearestVertex(srcObj.transform.position);
+        Vertex dst = GetNearestVertex(dstObj.transform.position);
+        BinaryHeap<Edge> frontier = new BinaryHeap<Edge>();
+        Edge[] edges;
+        Edge node, child;
+        int size = vertices.Count;
+        float[] distValue = new float[size];
+        int[] previous = new int[size];
+
+        //add the source node to the priority queue and assign a distance value 
+        //of infinity to all of them but the source node
+        node = new Edge(src, 0);
+        frontier.Add(node);
+        distValue[src.id] = 0;
+        previous[src.id] = src.id;
+        for (int i = 0; i < size; i++) {
+            if (i == src.id) {
+                continue;
+            }
+            distValue[i] = Mathf.Infinity;
+            previous[i] = -1;
+        }
+
+        //define a loop to iterate while the queue is not empty
+        while (frontier.Count != 0) {
+            node = frontier.Remove();
+            int nodeId = node.vertex.id;
+            //call the procedure to build the path when arriving at the destination
+            if (ReferenceEquals(node.vertex, dst)) {
+                return BuildPath(src.id, node.vertex.id, ref previous);
+            }
+            //otherwise process the visited nodes and add its neighbours to the
+            //queue, and return the path(not empty if there is a path from the
+            //source to the destination vertex
+            edges = GetEdges(node.vertex);
+            foreach (Edge e in edges) {
+                int eId = e.vertex.id;
+                if (previous[eId] != -1) {
+                    continue;
+                }
+                float cost = distValue[nodeId] + e.cost;
+                if (cost < distValue[e.vertex.id]) {
+                    distValue[eId] = cost;
+                    previous[eId] = nodeId;
+                    frontier.Remove(e);
+                    child = new Edge(e.vertex, cost);
+                    frontier.Add(child);
+                }
+            }
+        }
+
+        return new List<Vertex>();
+    }
+
     private List<Vertex> BuildPath(int srcId, int dstId, ref int[] prevList) {
         List<Vertex> path = new List<Vertex>();
         int prev = dstId;
