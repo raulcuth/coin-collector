8bc4119edaa5d6b5c68349892836589dc15a0a4d
raul_cuth <raul.cuth@gmail.com>
2019-06-11 20:33:23 +0300

    added sound emitter and receiver scripts

---
+++ Assets/Scripts/Stimuli/SoundEmitter.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundEmitter : MonoBehaviour {
+    public float soundIntensity;
+    public float soundAttenuation;
+    public GameObject emitterObject;
+    private Dictionary<int, SoundReceiver> receiverDictionary;
+
+    private void Start() {
+        receiverDictionary = new Dictionary<int, SoundReceiver>();
+        if (emitterObject == null) {
+            emitterObject = gameObject;
+        }
+    }
+
+    private void OnTriggerEnter(Collider coll) {
+        SoundReceiver receiver = coll.gameObject.GetComponent<SoundReceiver>();
+        if (receiver == null) {
+            return;
+        }
+        int objId = coll.gameObject.GetInstanceID();
+        receiverDictionary.Add(objId, receiver);
+    }
+
+    private void OnTriggerExit(Collider coll) {
+        SoundReceiver receiver = coll.gameObject.GetComponent<SoundReceiver>();
+        if (receiver == null) {
+            return;
+        }
+        int objId = coll.gameObject.GetInstanceID();
+        receiverDictionary.Remove(objId);
+    }
+
+    public void Emit() {
+        GameObject srObj;
+        Vector3 srPos;
+        float intensity;
+        float distance;
+        Vector3 emitterPos = emitterObject.transform.position;
+        //compute sound attenuation for receiver
+        foreach (SoundReceiver sr in receiverDictionary.Values) {
+            srObj = sr.gameObject;
+            srPos = srObj.transform.position;
+            distance = Vector3.Distance(srPos, emitterPos);
+            intensity = soundIntensity;
+            intensity -= soundAttenuation * distance;
+            if (intensity < sr.soundThreshold) {
+                continue;
+            }
+            sr.Receive(intensity, emitterPos);
+        }
+    }
+
+}
---
+++ Assets/Scripts/Stimuli/SoundReceiver.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundReceiver : MonoBehaviour {
+    public float soundThreshold;
+
+    public virtual void Receive(float intensity, Vector3 position) {
+        //TODO
+        //code behaviour
+        Debug.LogFormat("---> Can hear sound from: {0} with intensity: {1}", position, intensity);
+    }
+}
