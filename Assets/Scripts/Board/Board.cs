fd0a5708ebd66747fb8e8e18398e524211aad1fe
raulcuth <raul.cuth@gmail.com>
2019-06-14 20:54:17 +0300

    added backbone for board AI

---
+++ Assets/Scripts/Board/Board.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board {
+    protected int player;
+
+    public Board() {
+        player = 1;
+    }
+
+    //retrieve next possible moves
+    public virtual Move[] GetMoves() {
+        return new Move[0];
+    }
+
+    //play the move on the board
+    public virtual Board MakeMove(Move m) {
+        return new Board();
+    }
+
+    public virtual bool IsGameOver() {
+        return true;
+    }
+
+    public virtual int GetCurrentPlayer() {
+        return player;
+    }
+
+    //test the board's value for a given player
+    public virtual float Evaluate(int player) {
+        return Mathf.NegativeInfinity;
+    }
+
+    //test the board's value for the current player
+    public virtual float Evaluate() {
+        return Mathf.NegativeInfinity;
+    }
+}
---
+++ Assets/Scripts/Board/BoardAI.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardAI {
+    //works like a bounded DFS, in each step the move is chosen by selecting the option that
+    //maximizes the player's score and assuming that the opponent will choose the
+    //option of minimizing it until a terminal node is reached
+    public static float Minimax(Board board,
+                                int player,
+                                int maxDepth,
+                                int currentDepth,
+                                ref Move bestMove) {
+        if (board.IsGameOver() || currentDepth == maxDepth) {
+            return board.Evaluate(player);
+        }
+        bestMove = null;
+        float bestScore = Mathf.Infinity;
+        if (board.GetCurrentPlayer() == player) {
+            bestScore = Mathf.NegativeInfinity;
+        }
+        //loop through all possible moves and return the best score
+        foreach (Move m in board.GetMoves()) {
+            //create a new game state from the current move
+            Board boardNewState = board.MakeMove(m);
+            float currentScore;
+            Move currentMove = null;
+            //start the recursion
+            currentScore = Minimax(boardNewState, player, maxDepth, currentDepth + 1, ref currentMove);
+            //validate score for the current player
+            if (board.GetCurrentPlayer() == player) {
+                if (currentScore > bestScore) {
+                    bestScore = currentScore;
+                    bestMove = currentMove;
+                }
+            } else if (currentScore < bestScore) {
+                //validate the score for the adversary
+                bestScore = currentScore;
+                bestMove = currentMove;
+            }
+        }
+        return bestScore;
+    }
+}
