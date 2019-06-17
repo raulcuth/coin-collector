d975c61e4cbce73d0b4976e894aebbbfe0daa09c
raulcuth <raul.cuth@gmail.com>
2019-06-17 10:17:22 +0300

    added simple naive bayes prediction algorithm

---
+++ Assets/Scripts/MachineLearning/NBCLabel.cs
@@ -0,0 +1,4 @@
+﻿public enum NBCLabel {
+    POSITIVE,
+    NEGATIVE
+}
---
+++ Assets/Scripts/MachineLearning/NaiveBayesClassifier.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NaiveBayesClassifier : MonoBehaviour {
+    public int numAttributes;
+    public int numExamplesPositive;
+    public int numExamplesNegative;
+
+    public List<bool> attrCountPositive;
+    public List<bool> attrCountNegative;
+
+    private void Awake() {
+        attrCountPositive = new List<bool>();
+        attrCountNegative = new List<bool>();
+    }
+
+    public void UpdateClassifier(bool[] attributes, NBCLabel label) {
+        if (label == NBCLabel.POSITIVE) {
+            numExamplesPositive++;
+            attrCountPositive.AddRange(attributes);
+        } else {
+            numExamplesNegative++;
+            attrCountNegative.AddRange(attributes);
+        }
+    }
+
+    public float NaiveProbabilities(ref bool[] attributes,
+                                    bool[] counts,
+                                    float m,
+                                    float n) {
+        float prior = m / (m + n);
+        float p = 1f;
+        for (int i = 0; i < numAttributes; i++) {
+            p /= m;
+            if (attributes[i] == true) {
+                p *= counts[i].GetHashCode();
+            } else {
+                p *= m - counts[i].GetHashCode();
+            }
+        }
+        return prior * p;
+    }
+
+    public bool Predict(bool[] attributes) {
+        float nep = numExamplesPositive;
+        float nen = numExamplesNegative;
+        float x = NaiveProbabilities(ref attributes, attrCountPositive.ToArray(), nep, nen);
+        float y = NaiveProbabilities(ref attributes, attrCountNegative.ToArray(), nep, nen);
+        if (x >= y) {
+            return true;
+        }
+        return false;
+    }
+}
