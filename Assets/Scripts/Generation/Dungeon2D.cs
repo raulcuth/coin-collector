0f5d756a5303fd072577b8ab96b515cf4781c0ea
raul_cuth <raul.cuth@gmail.com>
2019-06-18 23:00:29 +0300

    added simple procedural dungeon generation algorithm

---
+++ Assets/Scripts/Generation/Dungeon2D.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dungeon2D : MonoBehaviour {
+    public float minAcceptSize;
+    public Rect area;
+    public Dictionary<int, List<DungeonNode2D>> tree;
+    public HashSet<DungeonNode2D> leaves;
+    public delegate Rect[] Split(Rect area);
+    public Split splitCall;
+    public DungeonNode2D root;
+
+    public void Init() {
+        leaves.Clear();
+        tree.Clear();
+        if (splitCall == null) {
+            splitCall = SplitNode;
+        }
+        root = new DungeonNode2D(area, this);
+    }
+
+    public void Build() {
+        root.Split(splitCall);
+        foreach (DungeonNode2D node in leaves) {
+            node.CreateBlock();
+        }
+    }
+
+    private void Awake() {
+        tree = new Dictionary<int, List<DungeonNode2D>>();
+        leaves = new HashSet<DungeonNode2D>();
+    }
+
+    public Rect[] SplitNode(Rect area) {
+        Rect[] areas = null;
+        DungeonNode2D[] children = null;
+        //check whether the area's width or height are below the minimum. If so, return that area
+        float value = Mathf.Min(area.width, area.height);
+        if (value < minAcceptSize) {
+            return areas;
+        }
+        //check the greater value between the width and height
+        areas = new Rect[2];
+        bool isHeightMax = area.height > area.width;
+        float half;
+
+        //split by height, if the height is the maximum value
+        if (isHeightMax) {
+            half = area.height / 2f;
+            areas[0] = new Rect(area);
+            areas[0].height = half;
+            areas[1] = new Rect(area);
+            areas[1].y = areas[0].y + areas[0].height;
+        } else {
+            half = area.width / 2f;
+            areas[0] = new Rect(area);
+            areas[0].width = half;
+            areas[1] = new Rect(area);
+            areas[1].x = areas[0].x + areas[0].width;
+        }
+        return areas;
+    }
+}
---
+++ Assets/Scripts/Generation/DungeonNode2D.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class DungeonNode2D {
+    public Rect area;
+    public Rect block;
+    public Dungeon2D root;
+    public DungeonNode2D left;
+    public DungeonNode2D right;
+    protected int depth;
+
+    public DungeonNode2D(Rect area, Dungeon2D root, int depth = 0) {
+        this.area = area;
+        this.root = root;
+        this.depth = depth;
+        this.root.leaves.Add(this);
+        if (!this.root.tree.ContainsKey(depth)) {
+            this.root.tree.Add(depth, new List<DungeonNode2D>());
+        }
+        this.root.tree[depth].Add(this);
+    }
+
+    public void Split(Dungeon2D.Split splitCall) {
+        this.root.leaves.Remove(this);
+        Rect[] areas = splitCall(area);
+        if (areas == null) {
+            return;
+        }
+        left = new DungeonNode2D(areas[0], root, depth + 1);
+        right = new DungeonNode2D(areas[1], root, depth + 1);
+    }
+
+    public void CreateBlock() {
+        block = new Rect();
+        block.xMin = Random.Range(area.xMin, area.center.x);
+        block.yMin = Random.Range(area.yMin, area.center.y);
+        block.xMax = Random.Range(area.center.x, area.xMax);
+        block.yMax = Random.Range(area.center.y, area.yMax);
+    }
+}
